0292dc92a324f6fa05753b43b94734d5261c1046
[haeun-d] <[haeun416@duksung.ac.kr]>
2025-06-15 00:43:45 +0900

    중복 HotIssue 제거 및 Pretendard 적용 오류 수정

---
+++ src/styles/font.ts
@@ -0,0 +1,10 @@
+const FONT = {
+    sm: { fontSize: "12px", fontWeight: "400" },
+    md: { fontSize: "14px", fontWeight: "500" },
+    lg: { fontSize: "16px", fontWeight: "600" },
+    xl: { fontSize: "20px", fontWeight: "700" },
+    xxl: { fontSize: "24px", fontWeight: "800" },
+  };
+  
+  export default FONT;
+  
---
+++ src/styles/theme.ts
@@ -0,0 +1,22 @@
+const theme = {
+    color: {
+      gray00: "#ffffff",
+      gray05: "#f8f9fa",
+      gray10: "#f1f3f5",
+      gray20: "#e9ecef",
+      gray30: "#dee2e6",
+      gray40: "#ced4da",
+      gray50: "#adb5bd",
+      gray60: "#868e96",
+      gray70: "#495057",
+      gray80: "#343a40",
+      primary10: "#e0f2ff",
+      primary20: "#b6e3ff",
+      primary70: "#339af0",
+      warning: "#fa5252",
+    },
+  };
+  
+  export default theme;
+  export type ThemeType = typeof theme;  
+  
