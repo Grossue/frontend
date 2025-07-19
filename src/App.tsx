e722c12045bd09d25dd8fb9ed0d7ef33df356ddd
Yejin <kyj5840kyj@duksung.ac.kr>
2025-07-19 21:37:03 +0900

    #17 Feat: 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import Sidebar from "./components/layout/Sidebar";
 import IssueReadingPage from "./pages/IssueReadingPage";
 import HotIssuePage from "./pages/HotIssuePage";
 import IssueLoadingPage from "./pages/IssueLoadingPage";
-
+import LoginPage from "./pages/LoginPage";
+import SignUp1Page from "./pages/SignUp1Page";
+import SignUp2Page from "./pages/SignUp2Page";
+import SignUp3Page from "./pages/SignUp3Page";
+import SignUp4Page from "./pages/SignUp4Page";
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +18,11 @@ function App() {
           <Route path="/" element={<HotIssuePage />} />
           <Route path="/reading" element={<IssueReadingPage />} />
           <Route path="/loading" element={<IssueLoadingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup1" element={<SignUp1Page />} />
+          <Route path="/signup2" element={<SignUp2Page />} />
+          <Route path="/signup3" element={<SignUp3Page />} />
+          <Route path="/signup4" element={<SignUp4Page />} />
         </Routes>
         <Sidebar />
       </div>
