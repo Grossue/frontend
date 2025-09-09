8e0d2cbdf07647499a202e81d067393ec91e5319
Yejin <kyj5840kyj@duksung.ac.kr>
2025-09-09 18:34:00 +0900

    #35 Feat: 유저 관련 api 세팅

--- src/api/User.ts
+++ src/api/User.ts
@@ -7,6 +7,7 @@ export const postRegister = async (data: {
   gender: "MALE" | "FEMALE";
   birthYear: string;
   level: "LEVEL1" | "LEVEL2";
+  schoolId: string;
 }) => {
   try {
     const res = await request.post({
@@ -20,7 +21,6 @@ export const postRegister = async (data: {
     throw error;
   }
 };
-
 export const postLogin = async (data: { email: string; password: string }) => {
   try {
     const res = await request.post({
@@ -31,6 +31,46 @@ export const postLogin = async (data: { email: string; password: string }) => {
     return res;
   } catch (error) {
     console.error("일반 로그인 오류:", error);
+    throw error;
+  }
+};
+export const getUserCheckNickname = async (nickname: string) => {
+  try {
+    const res = await request.get({
+      url: `/v1/user/checkNickname?nickname=${nickname}`,
+      params: {},
+    });
+    console.log("닉네임 중복 검사 완료", res);
+    return res;
+  } catch (error) {
+    console.error("닉네임 중복 검사 오류:", error);
+    throw error;
+  }
+};
+export const getUserCheckEmail = async (email: string) => {
+  try {
+    const res = await request.get({
+      url: `/v1/user/checkEmail?email=${email}`,
+      params: {},
+    });
+    console.log("이메일 중복 검사 완료", res);
+    return res;
+  } catch (error) {
+    console.error("이메일 중복 검사 오류:", error);
+    throw error;
+  }
+};
+
+export const getUser = async () => {
+  try {
+    const res = await request.get({
+      url: `/v1/user`,
+      params: {},
+    });
+    console.log("유저 정보 조회 완료", res);
+    return res;
+  } catch (error) {
+    console.error("유저 정보 조회 오류:", error);
     throw error;
   }
 };
