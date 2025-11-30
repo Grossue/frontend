e6422275280fe83decbfc6e6bdbee56abc54227a
Yejin <kyj5840kyj@duksung.ac.kr>
2025-11-30 19:03:59 +0900

    Fix: 뉴스 api 날짜 수정

--- src/api/Reading.ts
+++ src/api/Reading.ts
@@ -93,7 +93,7 @@ export const getArticleQna = async (sessionId: string, question: string) => {
 export const getArticleIssues = async (category: string) => {
   try {
     const res = await request.get({
-      url: `/v1/article/issues?category=${category}&date=2025-10-30`,
+      url: `/v1/article/issues?category=${category}&date=2025-11-29`,
       params: {},
     });
     console.log("카테고리별 이슈 목록 성공", res);
